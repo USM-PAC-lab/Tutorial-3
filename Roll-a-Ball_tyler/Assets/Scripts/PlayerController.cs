d3d4a675f65317fe2884ffe0ef60de2ab22aaceb
tyler <tyler.surber3@gmail.com>
2020-08-21 14:24:13 -0500

    Vid 6

--- Roll-a-Ball_tyler/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball_tyler/Assets/Scripts/PlayerController.cs
@@ -13,14 +13,24 @@ public class PlayerController : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce (movement * speed);
+        rb.AddForce(movement * speed);
+
     }
-    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+
+    }
+
 }
