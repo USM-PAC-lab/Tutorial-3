c88ce41e31f4897dfc787a3584e84b21714ddab9
cth33115 <catherine.dowell94@gamil.com>
2020-08-23 20:28:31 -0500

    done

---
+++ roll_a_ball-Dowell/Assets/Scripts/playercontroller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playercontroller : MonoBehaviour {
+
+	public float speed;
+	public Text counttext;
+	public Text wintext;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+		{
+			rb = GetComponent<Rigidbody>();
+			count = 0;
+			Setcounttext ();
+			wintext.text = "";
+		}
+
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("pick up"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			Setcounttext ();
+
+		}
+	}
+	void Setcounttext ()
+	{
+		counttext.text = "count: " + count.ToString ();
+		if (count >= 12)
+		{
+			wintext.text = "YOU WIN!";
+		}
+	}
+}
